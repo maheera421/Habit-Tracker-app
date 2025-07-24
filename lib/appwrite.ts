53a8607290b7c686f9ffb0e1837bc7d3c61a5fb5
Maheera Maryam <maheeramaryam95@gmail.com>
2025-07-25 01:39:58 +0500

    Today's habit page implemented with scrollable view.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -11,3 +11,8 @@ export const databases = new Databases(client);
 // creating variables for ids to access frequently
 export const DATABASE_ID = process.env.EXPO_PUBLIC_DB_ID!;
 export const HABITS_COLLECTION_ID = process.env.EXPO_PUBLIC_HABITS_COLLECTION_ID!;
+
+export interface RealtimeResponse {
+    events: string[];
+    payload: any;
+}
